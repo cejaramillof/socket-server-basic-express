e22b1c8aa9224f3e76353596e87f06cb686b30e9
Carlos Jaramillo <cejaramillof@gmail.com>
2021-01-09 17:08:40 -0500

    create models to band service

--- models/sockets.js
+++ models/sockets.js
@@ -1,39 +1,39 @@
 class Sockets {
-    constructor(io) {
-        this.io = io;
-        this.socketEvents();
-    }
+  constructor(io) {
+    this.io = io;
+    this.socketEvents();
+  }
 
-    socketEvents() {
-        // on connection
-        this.io.on('connection', (socket) => {
-            // socket or client (is the user connected)
-            console.log('Client connected! ClientId:', socket.id)
+  socketEvents() {
+    // on connection
+    this.io.on('connection', (socket) => {
+      // socket or client (is the user connected)
+      console.log('Client connected! ClientId:', socket.id)
 
-            // emit event (event, payload to share with client)
-            socket.emit('welcome-message', 'Welcome to da server');
+      // emit event (event, payload to share with client)
+      socket.emit('welcome-message', 'Welcome to da server');
 
-            // is most usually share objects not (strings, booleans)
-            socket.emit('welcome-object', {
-                msg: 'Welcome to da server',
-                serverDate: Date.now()
-            });
+      // is most usually share objects not (strings, booleans)
+      socket.emit('welcome-object', {
+        msg: 'Welcome to da server',
+        serverDate: Date.now()
+      });
 
-            // listen default namespace '/'
-            socket.on('mensaje-to-server', ({ msg, clientDate }) => {
-                console.log('Message from client:', msg, clientDate)
-            });
+      // listen default namespace '/'
+      socket.on('mensaje-to-server', ({ msg, clientDate }) => {
+        console.log('Message from client:', msg, clientDate)
+      });
 
-            socket.on('new-msg', ({ msg }) => {
-                console.log('New message:', msg)
-                // socket.emit('new-server-msg', { msg })
-                // only will notify current client (socket)
+      socket.on('new-msg', ({ msg }) => {
+        console.log('New message:', msg)
+        // socket.emit('new-server-msg', { msg })
+        // only will notify current client (socket)
 
-                this.io.emit('new-server-msg', { msg })
-                // will notify all sockets (clients connecteds)
-            });
-        });
-    }
+        this.io.emit('new-server-msg', { msg })
+        // will notify all sockets (clients connecteds)
+      });
+    });
+  }
 
 
 }
