74a3be217a11227f3671187aa5baf864b7d524d4
Carlos Jaramillo <cejaramillof@gmail.com>
2021-02-20 07:46:13 -0500

    Update sockets.js

--- chat/models/sockets.js
+++ chat/models/sockets.js
@@ -17,23 +17,21 @@ class Sockets {
         return socket.disconnect();
       }
       await usuarioConectado(uid);
+      
+      /*
+        socket.join('gamer-room'); // will join current socket to this room
+        this.io.to('gamer-room').emit(''); // will dispatch this event to all sockets subscribeds
+      */
       socket.join(uid);
 
-      // TODO: Validar el JWT // Si el token no es válido, desconectar
-      // TODO: Saber que usuario está activo mediante el UID
-      // TODO: Emitir todos los usuarios conectados
       this.io.emit('lista-usuarios', await getUsuarios())
 
-      // TODO: Socket join, uid
-      // TODO: Escuchar cuando el cliente manda un mensaje
       socket.on('mensaje-personal', async (payload) => {
         const mensaje = await grabarMensaje(payload);
         this.io.to(payload.para).emit('mensaje-personal', mensaje);
         this.io.to(payload.de).emit('mensaje-personal', mensaje);
       });
 
-      // TODO: Disconnect // Marcar en la BD que el usuario se desconecto
-      // TODO: Emitir todos los usuarios conectados
       socket.on('disconnect', async () => {
         await usuarioDesconectado(uid);
         this.io.emit('lista-usuarios', await getUsuarios())
